5413b13ee13868cc480c7046d984c3513187e018
Pauline-Oraro <hiltrapauline@gmail.com>
2025-07-02 15:02:50 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# BankHub - A Modern React App
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+
+
+const App = () => {
+  return(
+    <div className='text-amber-800 font-bold font-poppins'>BankHub</div>
+  )
+}
+
+export default App
