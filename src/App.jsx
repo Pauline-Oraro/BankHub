4982e5f9af4dba3e3c0ac1ed5769a4d4f0dfaf69
Pauline-Oraro <hiltrapauline@gmail.com>
2025-07-03 15:16:17 +0300

    css files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
-
-
+import styles from "./styles"
+import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
 
 const App = () => {
   return(
-    <div className='text-amber-800 font-bold font-poppins'>BankHub</div>
+    <div className='bg-primary w-full overflow-hidden'>
+      {/*wrapping the navbar */}
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar/>
+        </div>
+      </div>
+
+      {/*hero section */}
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero/>
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats/>
+          <Business/>
+          <Billing/>
+          <CardDeal/>
+          <Testimonials/>
+          <Clients/>
+          <CTA/>
+          <Footer/>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
